3194019563659ae907584844ab4c715d9c5d9951
marufurrahman1997@gmail.com <marufurrahman1997@gmail.com>
2018-03-11 08:56:05 +0600

    if_else statment

---
+++ learn.php
@@ -0,0 +1,14 @@
+<?php
+
+$maruf = 21;
+$mehedi = 21;
+
+if ($maruf > $mehedi) {
+	echo "Maruf is older then Mehedi.";
+} elseif ($maruf == $mehedi) {
+	echo "Maruf and Mehedi are the same age.";
+} else {
+	echo "Maruf is smaller the Mehedi";
+}
+
+?>
